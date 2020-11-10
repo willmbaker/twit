ffc10ca32387dada954a1a1e64ced32b352c44e0
Will Baker <will.baker@serato.com>
2020-11-11 01:09:18 +1300

    [1] [Git] I can open a repository
    @?- Yeah, but not really. It's... I guess it is a shim? "a washer or thin strip of material used to align parts, make them fit, or reduce wear."... pretty close, I guess ;)

--- GitRepository_test.h
+++ GitRepository_test.h
@@ -12,4 +12,11 @@ using namespace twit;
 TEST( GitRepository, i_can_open_a_git_repository )
 {
 	GitRepository repository;
+	EXPECT_FALSE( repository.is_open() );
+
+	repository.open( "/Users/will.baker/coin/muse" );
+	EXPECT_TRUE( repository.is_open() );
+	
+	repository.close();
+	EXPECT_FALSE( repository.is_open() );
 }
